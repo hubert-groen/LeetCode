f10a44af63110bce0e46164496a1e45ed59a966e
itsMe <61231501+hubert-groen@users.noreply.github.com>
2023-12-12 20:30:48 +0100

    Time: 37 ms (81.92%), Space: 16.4 MB (7.73%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        
+        output = ''
+
+        c = int(a, 2) + int(b, 2)
+        
+        if c == 0:
+            return str(0)
+
+        while c > 0:
+            
+            output = str(c%2) + output
+            c = c//2
+        
+        return output
