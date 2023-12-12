d6738fc12cbd0cbadc96af04758d626213c8eaeb
itsMe <61231501+hubert-groen@users.noreply.github.com>
2023-12-12 20:57:21 +0100

    Time: 66 ms (15.44%), Space: 16.3 MB (41.97%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -5,7 +5,7 @@ class Solution:
         second_max_index = -2
 
         if len(nums) == 2:
-            return (nums[max_index]-1)* (nums[second_max_index]-1)
+            return (nums[max_index]-1)*(nums[second_max_index]-1)
 
         for i in range(len(nums)):
 
@@ -16,4 +16,4 @@ class Solution:
             elif nums[i] > nums[second_max_index]:
                 second_max_index = i
 
-        return (nums[max_index]-1)* (nums[second_max_index]-1)
+        return (nums[max_index]-1)*(nums[second_max_index]-1)
