7ff2de482bef723573a2c5836bbbac8ce0698061
itsMe <61231501+hubert-groen@users.noreply.github.com>
2023-12-14 11:38:54 +0100

    Time: 33 ms (83.85%), Space: 16.3 MB (15.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,8 +5,11 @@ class Solution:
         
         def fib(n):
         
-            if n <=1:
-                return 1
+            # if n <=1:         # to też pasuje
+            #     return 1      # ale poniższe bardziej intuicyjne
+            
+            if n <= 2:
+                return n
 
             elif n not in memo:
                 memo[n] = fib(n-1) + fib(n-2)
