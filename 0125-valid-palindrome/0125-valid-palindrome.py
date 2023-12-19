a365f8a37de9cdcaa96d3e555440b91939def52b
itsMe <61231501+hubert-groen@users.noreply.github.com>
2023-12-19 23:57:12 +0100

    Time: 41 ms (93.26%), Space: 17 MB (94.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,38 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        def ascii(char):
-            
-            return 'A' <= char <= 'Z' or 'a' <= char <= 'z' or '0' <= char <= '9'
-
-
-        start_pointer = 0
-        end_pointer = len(s) - 1
-
-
-        while start_pointer < end_pointer:
-
-            if not ascii(s[start_pointer]):
-                start_pointer += 1
-                continue
-
-            if not ascii(s[end_pointer]):
-                end_pointer -= 1
-                continue
-
-            ss = s[start_pointer]
-            es = s[end_pointer]
-
-            if s[start_pointer].lower() != s[end_pointer].lower() :
-                return False
-            
-            start_pointer += 1
-            end_pointer -= 1
-
-            if start_pointer > end_pointer:
-                return True
-
-        if start_pointer > end_pointer:
-            return False
-            
-        return True    
+        string = ''
+        
+        for i in s:
+            if i.isalnum() == True:
+                string += i.lower()
+        
+        return string == string[::-1]
